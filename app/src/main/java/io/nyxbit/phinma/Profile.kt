f15be8af4d98217090b893b57ce6afe8bead0466
Dominic Mendoza Enriquez <dome.enriquez.up@phinmaed.com>
2024-08-12 17:22:47 +0800

    Foo

--- app/src/main/java/io/nyxbit/phinma/Profile.kt
+++ app/src/main/java/io/nyxbit/phinma/Profile.kt
@@ -3,6 +3,7 @@ package io.nyxbit.phinma
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -31,22 +32,6 @@ class Profile : Fragment() {
             "Init"-> View.GONE
             else -> View.GONE
         }
-
-
-
-        binding.mobileNumber.addTextChangedListener(object: TextWatcher{
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                TODO("Not yet implemented")
-            }
-
-        })
 
 
         binding.fullName.setText(viewModel.fullName)
